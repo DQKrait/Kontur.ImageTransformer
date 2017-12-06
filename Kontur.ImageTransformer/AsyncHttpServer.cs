afae06782e7e5599a58b3560b0cb0c1423a644f9
Krait <krait@skbkontur.ru>
2017-12-06 10:26:37 +0500

    Initial.

---
+++ Kontur.ImageTransformer/AsyncHttpServer.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Kontur.ImageTransformer
+{
+    internal class AsyncHttpServer : IDisposable
+    {
+        public AsyncHttpServer()
+        {
+            listener = new HttpListener();
+        }
+        
+        public void Start(string prefix)
+        {
+            lock (listener)
+            {
+                if (!isRunning)
+                {
+                    listener.Prefixes.Clear();
+                    listener.Prefixes.Add(prefix);
+                    listener.Start();
+
+                    listenerThread = new Thread(Listen)
+                    {
+                        IsBackground = true,
+                        Priority = ThreadPriority.Highest
+                    };
+                    listenerThread.Start();
+                    
+                    isRunning = true;
+                }
+            }
+        }
+
+        public void Stop()
+        {
+            lock (listener)
+            {
+                if (!isRunning)
+                    return;
+
+                listener.Stop();
+
+                listenerThread.Abort();
+                listenerThread.Join();
+                
+                isRunning = false;
+            }
+        }
+
+        public void Dispose()
+        {
+            if (disposed)
+                return;
+
+            disposed = true;
+
+            Stop();
+
+            listener.Close();
+        }
+        
+        private void Listen()
+        {
+            while (true)
+            {
+                try
+                {
+                    if (listener.IsListening)
+                    {
+                        var context = listener.GetContext();
+                        Task.Run(() => HandleContextAsync(context));
+                    }
+                    else Thread.Sleep(0);
+                }
+                catch (ThreadAbortException)
+                {
+                    return;
+                }
+                catch (Exception error)
+                {
+                    // TODO: log errors
+                }
+            }
+        }
+
+        private async Task HandleContextAsync(HttpListenerContext listenerContext)
+        {
+            // TODO: implement request handling
+
+            listenerContext.Response.StatusCode = (int)HttpStatusCode.OK;
+            using (var writer = new StreamWriter(listenerContext.Response.OutputStream))
+                writer.WriteLine("Hello, world!");
+        }
+
+        private readonly HttpListener listener;
+
+        private Thread listenerThread;
+        private bool disposed;
+        private volatile bool isRunning;
+    }
+}
---
+++ Kontur.ImageTransformer/EntryPoint.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Kontur.ImageTransformer
+{
+    public class EntryPoint
+    {
+        public static void Main(string[] args)
+        {
+            using (var server = new AsyncHttpServer())
+            {
+                server.Start("http://+:8080/");
+
+                Console.ReadKey(true);
+            }
+        }
+    }
+}
